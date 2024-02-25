0677722979373a4b7f1266116c7d5eac41057f02
Wendi <weneze18@utexas.edu>
2024-02-25 00:47:01 -0600

    Added arrow code for attack
    - Added arrow art
    - Made Area2D for arrow and code for physics/movement
    - Implement attack function in player for arrow to shoot when attack (spacebar) is pressed

--- player.gd
+++ player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var speed = 400
 var walking = false
 var is_attacking = false
+@onready var end_of_bow = $Marker2D
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -12,6 +13,7 @@ func get_input():
 	if Input.is_action_just_pressed("attack"):
 		is_attacking = true
 		$AnimatedSprite2D.play("attack")
+		arrow()
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -37,6 +39,15 @@ func _physics_process(delta):
 	get_input()
 	move_and_slide()
 
+func arrow():
+	var arrow = preload("res://arrow.tscn").instantiate()
+	add_child(arrow)
+	arrow.global_position = end_of_bow.global_position  # Set the bullet's position
+	
+	var target = get_global_mouse_position()
+	var direction_to_mouse = arrow.global_position.direction_to(target).normalized()
+	
+	arrow.set_direction(direction_to_mouse)
 
 func _on_walk_timer_timeout():
 	$walk_sound.play()
