0311150074e1bb50b5e25235114e08fb8f8d7108
PotatoHungry <evannguyen630@gmail.com>
2024-02-26 11:35:25 -0600

    mob movement

--- mob_goblin.gd
+++ mob_goblin.gd
@@ -1,14 +1,31 @@
 extends RigidBody2D
 
-
+@export var speed = 100
+var player_position
+var target_position
+@onready var player = get_parent().get_node("Player")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
-	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
+	$AnimatedSprite2D.play("idle")
+	#$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	player_position = player.position
+	target_position = (player.position - position).normalized()
+	
+	if position.distance_to(player_position) > 200:
+		set_linear_velocity(target_position * speed)
+	else:
+		set_linear_velocity(Vector2.ZERO)
+		$AnimatedSprite2D.play("attack")
+		
+	if target_position.x > 0:
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = true
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
