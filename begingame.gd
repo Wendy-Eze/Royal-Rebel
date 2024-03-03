7d5703b25238b6ac0a993c176449a1de1307deb9
Wendi <weneze18@utexas.edu>
2024-03-03 05:57:25 -0600

    Added opening scene to main scene, modified opening scene layout, and created a flow of all scenes

--- begingame.gd
+++ begingame.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
-
-const LevelBegin = "res://level_"
+signal player_near
+#const LevelBegin = "res://level_"
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		print("begun game")
-		get_tree().change_scene_to_file("res://level_1.tscn")
+		print("player wanting to start game")
+		#get_tree().change_scene_to_file("res://level_1.tscn")
+		player_near.emit()
 
