2b47170bc03f92bd0e6dcd70de3f6d9902427e88
Wendi <weneze18@utexas.edu>
2024-03-03 18:50:33 -0600

    Fixed opening_scene issue

--- Control.gd
+++ Control.gd
@@ -6,13 +6,13 @@ signal start_tut
 var dialogue_lines = ["The kingdom of Avalon has been under the stifling rule of the corrupt King Zephyr for many, many years. Citizens of Avalon have been struggling under the weight of increasing taxes and mass incarceration by the kingsmen.", "Anyone against his regime will be thrown into jail , never to see the light of day again. In the shadows, I, the Royal Rebel, bid my time to eventually overthrow the king and return peace to the kingdom.","News has spread that King Zyphyr's plan to turn everyone in Avalon into mindless puppets to do his bidding, using the power of a legendary diadem, thought only to be a myth.","Because the journey is too dangerous for others, I must travel through the kingdom and uncharted wilderness alone to find and destroy the diadem before the kingsmen find it. The legend of the diadem states it is hidden in the Sacred Willow Tree deep in the outskirts of the kingdom, guarded by a fierce dragon..."]
 var current_line = -1
 
-var dialogue_lines = [
-	"The kingdom of Avalon has been under the stifling rule of the corrupt King Zephyr for many, many years. Citizens of Avalon have been struggling under the weight of increasing taxes and mass incarceration by the kingsmen.",
-	"Anyone against his regime will be thrown into jail, never to see the light of day again. In the shadows, you, the Royal Rebel, bid your time to eventually overthrow the king and return peace to the kingdom.",
-	#"Among the king’s many victims, my young brother is locked in his dungeon.",
-	"News has spread that King Zephyr plans to turn everyone in Avalon into mindless puppets to do his bidding, using the power of a legendary Diadem, thought only to be a myth.",
-	"Because the journey is too dangerous for others, you must travel through the kingdom and uncharted wilderness alone to find and destroy the diadem before the kingsmen find it. The legend of the Diadem states it is hidden in the Sacred Willow Tree deep in the outskirts of the kingdom, guarded by a fierce dragon..."]
-var current_line = 0
+#var dialogue_lines = [
+	#"The kingdom of Avalon has been under the stifling rule of the corrupt King Zephyr for many, many years. Citizens of Avalon have been struggling under the weight of increasing taxes and mass incarceration by the kingsmen.",
+	#"Anyone against his regime will be thrown into jail, never to see the light of day again. In the shadows, you, the Royal Rebel, bid your time to eventually overthrow the king and return peace to the kingdom.",
+	##"Among the king’s many victims, my young brother is locked in his dungeon.",
+	#"News has spread that King Zephyr plans to turn everyone in Avalon into mindless puppets to do his bidding, using the power of a legendary Diadem, thought only to be a myth.",
+	#"Because the journey is too dangerous for others, you must travel through the kingdom and uncharted wilderness alone to find and destroy the diadem before the kingsmen find it. The legend of the Diadem states it is hidden in the Sacred Willow Tree deep in the outskirts of the kingdom, guarded by a fierce dragon..."]
+#var current_line = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -25,7 +25,7 @@ func advance_text():
 	current_line += 1
 
 	# Check if there are more lines of dialogue to display
-	if current_line < dialogue_lines.size() -1:
+	if current_line < dialogue_lines.size():
 		# Update the text in the Label node to display the next line of dialogue
 		$TextLabel.text = dialogue_lines[current_line]
 		print(current_line)
