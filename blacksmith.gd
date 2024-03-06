1756f46b272e8a6f43e38b02126513a75063e642
Wendi <weneze18@utexas.edu>
2024-03-06 17:14:44 -0600

    Made a market with npcs and dialogue

---
+++ blacksmith.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		$talk.show()
+
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		$talk.hide()
