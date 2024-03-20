532186de7f057f3c5a533747975f832b94cdc8e8
Wendi <weneze18@utexas.edu>
2024-03-20 15:34:27 -0500

    implemented area stuff

--- intro/goblinarea.gd
+++ intro/goblinarea.gd
@@ -1,8 +1,13 @@
 extends Area2D
 signal player_near
+signal player_out
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		print("player is near goblin")
 		player_near.emit()
 		
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		print("player not near goblin")
+		player_out.emit()
--- intro/storearea.gd
+++ intro/storearea.gd
@@ -1,8 +1,15 @@
 extends Area2D
 
 signal player_near
+signal player_out
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		print("player is near store")
 		player_near.emit()
+
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		print("player is near store")
+		player_out.emit()
